e4b6252bcc5a0755df7f467e6f793ed3ca001a76
Igor Kharchenko <i.s.kharchenko96@gmail.com>
2024-01-19 18:56:17 +0500

    first task

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,13 +1,16 @@
-const timer = document.getElementById("timer");
 
-let second = 59;
+const initialTimer = document.getElementById("timer");
+let seconds = Number(initialTimer.textContent);
+ initialTimer.textContent =  new Date(2023, 1, 1, 0, 0, seconds).toLocaleTimeString();
+ const link = document.getElementById("link"); 
+ function countdownTimer() {
+     seconds--;
+     initialTimer.textContent =  new Date(2023, 1, 1, 0, 0, seconds).toLocaleTimeString();
 
-function updateTimer () {
-    timerElement.textContent = second;
-    second--;
-
-    if (seconds < 0) {
-        clearInterval(interval);
-        alert("Вы победили в конкурсе!")
-    }
-}
+     if(seconds === 0) {
+         clearInterval(intervalId);
+         alert('Вы победили в конкурсе!');
+         link.click();
+     }
+ }
+const intervalId = setInterval(countdownTimer, 1000);
